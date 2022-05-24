33a4f3a6ebc647713bae53c793aba472b846739f
Daksha Ranjan <94025090+DakshaDuckie@users.noreply.github.com>
2022-05-24 16:22:02 -0700

    Add files via upload

---
+++ LetsChat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDuZ0a3vWOnYcrOmCkzrcJntCJ7z6ava6g",
+    authDomain: "letschat-web-app-7966b.firebaseapp.com",
+    databaseURL: "https://letschat-web-app-7966b-default-rtdb.firebaseio.com",
+    projectId: "letschat-web-app-7966b",
+    storageBucket: "letschat-web-app-7966b.appspot.com",
+    messagingSenderId: "331298027926",
+    appId: "1:331298027926:web:7d9b5a48e2365abf71c5b7",
+    measurementId: "G-LPZWP6BCT4"
+  };
+firebase.initializeApp(firebaseConfig);
+
